c30962634ec79447e3066d7ab972f06eec392cc2
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-11-12 10:11:08 +0100

    Add files via upload

---
+++ cartella.java
@@ -0,0 +1,21 @@
+package tombola;
+import java.util.*;
+public class cartella {
+	
+	int [][] cartella = new int [3][9];
+	
+	public void generaCartella() {
+		int count = 0;
+		for (int i = 0; i<9 ; i++)
+		{
+			Random exist = new Random();
+			while (count<5)
+			{
+				if (exist.nextBoolean())
+				{
+					if (i = 0)
+				}
+			}
+		}
+	}
+}
