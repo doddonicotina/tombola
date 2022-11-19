13ab118ab7414b5c9e1cfd30c8f52b410828c932
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-11-19 10:52:17 +0100

    Update cartella.java

--- cartella.java
+++ cartella.java
@@ -1,21 +1,51 @@
 package tombola;
 import java.util.*;
-public class cartella {
+public class Cartella {
 	
 	int [][] cartella = new int [3][9];
+	Random rand = new Random();
 	
-	public void generaCartella() {
-		int count = 0;
-		for (int i = 0; i<9 ; i++)
+	public Cartella() 
+	{
+		for (int i = 0; i<3; i++)
 		{
-			Random exist = new Random();
-			while (count<5)
+			for (int j = 0; j<9; j++)
 			{
-				if (exist.nextBoolean())
-				{
-					if (i = 0)
-				}
+				cartella[i][j]=0;
 			}
 		}
+		
+		for (int i = 0; i<3; i++)
+		{
+			int [] vet = sceltaBuchi();
+			for (int j = 0; j<vet.length; j++)
+			{
+				cartella[i][vet[j]]=rand.nextInt(vet[j]*10, vet[j]*10+9);
+			}
+		}
+		
 	}
+	
+	public int[] sceltaBuchi()
+	{
+		int [] vet = new int[5];
+		
+		for (int i=0; i<vet.length;i++)
+		{
+			//aggiungere controllo
+			vet[i] = rand.nextInt(9);
+		}
+		return vet;
+	}
+	
+	public void print()
+	{
+        for (int i = 0; i < cartella.length; i++) {
+        	System.out.println();
+     
+            for (int j = 0; j < cartella[i].length; j++)
+                System.out.print(cartella[i][j] + " ");
+	}}
 }
+ 
+
