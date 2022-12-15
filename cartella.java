033c4e886a408c4256f08559244f95dd45ec77d6
doddonicotina <74264293+doddonicotina@users.noreply.github.com>
2022-12-15 12:28:29 +0100

    Update cartella.java

--- cartella.java
+++ cartella.java
@@ -1,23 +1,38 @@
+package tombola;
 import java.util.*;
-
-import javax.swing.JButton;
 import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.plaf.InsetsUIResource;  
-public class Cartella {
+public class Cartella extends Canvas implements ActionListener {
 	
-	public int [][] cartella = new int [3][9];
+	int [][] cartella = new int [3][9];
+	JButton[][] ma = new JButton[3][9];
+	JFrame f = new JFrame ("Cartella");
 	Random rand = new Random();
 	int []numeri=new int[15];
 	int contatore=0;
 	
+	
+	ActionListener listener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() instanceof JButton) {
+            	JLabel label = new JLabel(); //JLabel Creation
+                label.setIcon(new ImageIcon("doddo.png")); //Sets the image to be displayed as an icon
+                label.setBounds(100, 100, 200, 200);
+                f.add(label);
+            }
+        }
+    };
 	public Cartella() 
 	{
 		for (int i = 0; i<3; i++)
@@ -50,7 +65,7 @@ public class Cartella {
 					{
 						do 
 						{
-							num=rand.nextInt();
+							num=rand.nextInt(vet[j]*10, vet[j]*10+9);
 						}while (contains(numeri, num));
 						
 						cartella[i][vet[j]]=num;
@@ -61,39 +76,67 @@ public class Cartella {
 			}
 		
 		}
+		
+		
         
-        JButton btn1 = new JButton(Integer.toString(cartella[0][0]));    
-        JButton btn2 = new JButton(cartella[0][1]);    
-        JButton btn3 = new JButton(cartella[0][2]);    
-        JButton btn4 = new JButton(cartella[0][3]);    
-        JButton btn5 = new JButton(cartella[0][4]);    
-        JButton btn6 = new JButton(cartella[0][5]);    
-        JButton btn7 = new JButton(cartella[0][6]);    
-        JButton btn8 = new JButton(cartella[0][7]);    
-        JButton btn9 = new JButton(cartella[0][8]);
-        JButton btn10 = new JButton(cartella[0][9]);    
-        JButton btn11 = new JButton(cartella[1][0]);    
-        JButton btn12 = new JButton(cartella[1][1]);    
-        JButton btn13 = new JButton(cartella[1][2]);    
-        JButton btn14 = new JButton(cartella[1][3]);    
-        JButton btn15 = new JButton(cartella[1][4]);    
-        JButton btn16 = new JButton(cartella[1][5]);    
-        JButton btn17 = new JButton(cartella[1][6]);    
-        JButton btn18 = new JButton(cartella[1][7]);
-        JButton btn19 = new JButton(cartella[1][8]);    
-        JButton btn20 = new JButton(cartella[1][9]);    
-        JButton btn21 = new JButton(cartella[2][0]);    
-        JButton btn22 = new JButton(cartella[2][1]);    
-        JButton btn23 = new JButton(cartella[2][2]);    
-        JButton btn24 = new JButton(cartella[2][3]);    
-        JButton btn25 = new JButton(cartella[2][4]);    
-        JButton btn26 = new JButton(cartella[2][5]);    
-        JButton btn27 = new JButton(cartella[2][6]);
-        JButton btn28 = new JButton(cartella[2][7]);
-        JButton btn29 = new JButton(cartella[2][8]);
-        JButton btn30 = new JButton(cartella[2][9]);
         
-	
+        Insets buttonmargin = new InsetsUIResource(0, 0, 0, 0);
+
+        for (int i=0; i<3;i++) {
+        	for (int j=0; j<9; j++) {
+        		ma[i][j] = new JButton();
+        		ma[i][j].setMargin(buttonmargin);
+        		ma[i][j].setFont(new Font(Font.SANS_SERIF, Font.BOLD, 15));
+        		ma[i][j].setBackground(Color.WHITE);
+        		ma[i][j].setText(Integer.toString(cartella[i][j]));
+        		ma[i][j].addActionListener(listener);
+        		f.add(ma[i][j]);
+        	}
+        }
+        
+        ImageIcon doddo = new ImageIcon("doddo.png");
+        ImageIcon gio = new ImageIcon("gio.png");
+        ma[0][0].setIcon(doddo);
+        /*
+        ma[0][0].addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {  
+                	ImageIcon doddo = new ImageIcon("doddo.png");
+                    ImageIcon gio = new ImageIcon("gio.png");
+
+                    for (int i=0; i<3;i++) {
+                    	for (int j=0; j<9; j++) {
+                    			if (e.getSource()==ma[i][j]) {
+                    				ma[i][j].setIcon(doddo);
+                    			}
+                    		}
+                    	}
+               	}  
+            });*/
+        
+        f.setBackground(Color.white);
+        f.setSize(900,400);  
+        f.setVisible(true);  
+        f.setLayout(new GridLayout(3,8));
+        
+        
+		
+	}
+	/*
+	@Override
+	public void actionPerformed (ActionEvent e) {
+		ImageIcon doddo = new ImageIcon("doddo.png");
+        ImageIcon gio = new ImageIcon("gio.png");
+
+        for (int i=0; i<3;i++) {
+        	for (int j=0; j<9; j++) {
+        			if (e.getSource()==ma[i][j]) {
+        				ma[i][j].setIcon(doddo);
+        			}
+        		}
+        	}
+	}*/
 	
 	public int[] sceltaBuchi()
 	{
@@ -142,5 +185,11 @@ public class Cartella {
 		return false;
 	}
 
-   
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
  
+
